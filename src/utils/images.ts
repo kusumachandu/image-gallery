dffd9b52d5d0071ee2aca62718e7baf58b527525
chandra shekar <kusumachandu43@gmail.com>
2023-07-20 07:12:34 +0530

    created an home page and displayed all the images

---
+++ src/utils/images.ts
@@ -0,0 +1,18 @@
+export const images = [
+  '/banglow.jpeg',
+  '/calf.jpeg',
+  '/cow-girls.jpeg',
+  '/cycle.jpeg',
+  '/dark-forest.jpeg',
+  '/deer.webp',
+  '/desert.webp',
+  '/flower.jpeg',
+  '/foody.jpeg',
+  'foreign-streets.jpeg',
+  '/long-roads.jpeg',
+  '/parachutes.webp',
+  '/mountains.jpeg',
+  '/police-station.jpeg',
+  '/rain-drops.webp',
+  '/runway.jpeg'
+]
